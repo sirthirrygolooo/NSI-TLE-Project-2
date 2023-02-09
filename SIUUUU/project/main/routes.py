f6d3ad9ccf0207f342e3e598eb4cada8e4fa0b75
sirthirrygolooo <72656353+sirthirrygolooo@users.noreply.github.com>
2023-02-09 12:58:49 +0100

    present. page

--- SIUUUU/project/main/routes.py
+++ SIUUUU/project/main/routes.py
@@ -5,16 +5,6 @@ main = Blueprint('main', __name__)
 from flask import Blueprint, render_template
 
 @main.route('/')
-def home():
-	posts = [
-		{
-			"body": "Hello, this is a post",
-			"timestamp": "This is a date and time",
-		}
-	]
+def presentation():
 	
-	return render_template("home.html", posts=posts)
-
-@main.route('/parallax')
-def parallax():
-	return render_template("parallax.html")
+	return render_template("presentation.html")
--- SIUUUU/project/static/js/script_para.js
+++ SIUUUU/project/static/js/script_para.js
@@ -1,8 +1,3 @@
-
-
-/* Please ❤ this if you like it! */
-
-
 (function($) { "use strict";
  
  	//Parallax            
@@ -25,35 +20,7 @@
         });
       });    
 	  }
-	scrollBanner();	              
-
-	//Page cursors
-
-    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
-        t.style.left = n.clientX + "px", 
-		t.style.top = n.clientY + "px", 
-		e.style.left = n.clientX + "px", 
-		e.style.top = n.clientY + "px", 
-		i.style.left = n.clientX + "px", 
-		i.style.top = n.clientY + "px"
-    });
-    var t = document.getElementById("cursor"),
-        e = document.getElementById("cursor2"),
-        i = document.getElementById("cursor3");
-    function n(t) {
-        e.classList.add("hover"), i.classList.add("hover")
-    }
-    function s(t) {
-        e.classList.remove("hover"), i.classList.remove("hover")
-    }
-    s();
-    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
-        o(r[a])
-    }
-    function o(t) {
-        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
-    }              
-              
+	scrollBanner();	                            
   
 //Scroll back to top
               
@@ -74,7 +41,7 @@ $(document).ready(function() {
 		})
   
   		
-		/* Hero Case study images */			
+		/* Hero Case picc */			
 		
 		$('.case-study-name:nth-child(1)').on('mouseenter', function() {
 			$('.case-study-name.active').removeClass('active');
@@ -104,4 +71,4 @@ $(document).ready(function() {
   
   });            
               
-})(jQuery); 
+})(jQuery);
