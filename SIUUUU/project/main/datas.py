3fe051e03fd5dce9781ebdffa5d416c638fe3beb
sirthirrygolooo <72656353+sirthirrygolooo@users.noreply.github.com>
2023-02-16 15:07:58 +0100

    dfgd

--- SIUUUU/project/main/datas.py
+++ SIUUUU/project/main/datas.py
@@ -1,11 +1,50 @@
-candidats = ['Jean Clanche','benoit jspquoi', 'tabar lis']
+candidats = [
+    {
+    'candidat': 'Jean Clanche',
+    'parti': 'PS',
+    'vote': 0
+    },
+    {
+    'candidat': 'benoit jspquoi',
+    'parti': 'PS',
+    'vote': 0
+    },
+    {
+    'candidat': 'tabar lis',
+    'parti': 'PS',
+    'vote': 0
+    }
+]
 
 def get_candidats(candidats):
-    if len(candidats) < 6 :
-        for k in range(6 - len(candidats)):
-            candidats.append('Candidat ' + str((6-len(candidats)+1)+2*k))
+    if len(candidats) > 6 :
+        return candidats[:6]
+    elif candidats == []:
+        for i in range(6):
+            candidats.append({
+                'candidat': 'Candidat '+str(i+1),
+                'parti': 'Inconnu',
+                'vote': 0
+            })
         return candidats
-    elif len(candidats) > 6:
-        return candidats[:6] 
-    else:
+    elif len(candidats) < 6 :
+        for i in range(6-len(candidats)):
+            candidats.append({
+                'candidat': 'Candidat '+str((6-len(candidats))+2*i+1),
+                'parti': 'Inconnu',
+                'vote': 0
+            })
         return candidats
+    else :
+        return candidats
+
+
+    
+
+    
+
+
+
+
+
+    
